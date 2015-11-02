1407e0fb58269b443bbac6b17c06efef61d5e52c
Bram Engel <engelenburg1980@gmail.com>
2015-11-02 17:51:17 +0100

    Tackled deprecation warning regarding NULL values for database timestamps

--- test/test_helper/database.rb
+++ test/test_helper/database.rb
@@ -7,12 +7,13 @@ ActiveRecord::Base.establish_connection config.merge("database" => nil)
 ActiveRecord::Base.connection.create_database database, {:charset => "utf8", :collation => "utf8_unicode_ci"} rescue nil
 ActiveRecord::Base.establish_connection config
 
+ActiveRecord::Base.connection.drop_table(:status_quo_events)
 ActiveRecord::Base.connection.create_table(:status_quo_events) do |t|
   t.string :resource
   t.string :segment
   t.datetime :moment
   t.string :status
-  t.timestamps
-end rescue nil
+  t.timestamps :null => false
+end
 
 ActiveRecord::Base.connection.truncate(:status_quo_events)
